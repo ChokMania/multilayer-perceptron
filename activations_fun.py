4b87fca8aaf853dd722ed6cf0d12b34b86efe79b
ChokMania <judumay@student.42.fr>
2021-01-14 12:38:40 +0100

    last version

--- activations_fun.py
+++ activations_fun.py
@@ -2,9 +2,10 @@ import numpy as np
 
 
 def sigmoid(x):
-	return(1 / (1 + np.exp(-x)))
+    return 1 / (1 + np.exp(-x))
+
 
 def softmax(x):
-	expo = np.exp(x)
-	expo_sum = np.sum(np.exp(x))
-	return expo / expo_sum
+    expo = np.exp(x)
+    expo_sum = np.sum(np.exp(x))
+    return expo / expo_sum
